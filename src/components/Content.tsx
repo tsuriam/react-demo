d08b33860864de3c89764903197653b099b6bc7a
Tsuria Mor <tsuria_m@ituran.com>
2024-05-01 16:40:19 +0300

    contnent

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,15 +2,10 @@ export default function Content() {
   return (
     <main role="main" className="inner cover">
       <h1 className="cover-heading">Cover your page.</h1>
-      <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-      <p className="lead">
+      <p className="lead" style={{width:500,fontSize:16}}>Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+      <p className="lead" style={{width:500,fontSize:16}}>
         <a href="#" className="btn btn-lg btn-secondary">Learn more</a>
       </p>
-    </main>)
-    <div>
-      <h3>Cover your page.</h3>
-      <div style={{width:500,fontSize:16}}>Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</div>   
-    
-    </div>
+    </main>
   )
 }
