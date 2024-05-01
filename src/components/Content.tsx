6376d2ac11434e6268051aea7b39cbe37e78b361
Hen <hen_elk@ituran.com>
2024-05-01 16:39:17 +0300

    changes

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,9 @@
 export default function Content() {
   return (
-    <div>content</div>
+    <div>
+      <h3>Cover your page.</h3>
+      <div style={{width:500,fontSize:16}}>Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</div>   
+    
+    </div>
   )
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,9 @@
 export default function Menu()
 {
-    return <div>Menu</div>
+    return (
+    <div>
+        <a href="" className="link">Home</a>
+        <a href="" className="link">Features</a>
+        <a href="" className="link">Contact</a>
+    </div>)
 }
