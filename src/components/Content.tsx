1ad0a6020d798df40dc22be6d0fa3b996c4a11ae
Tsuria Mor <tsuria_m@ituran.com>
2024-05-01 16:37:11 +0300

    aaa

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,10 @@
 export default function Content() {
   return (
-    <div>content</div>
-  )
+    <main role="main" className="inner cover">
+      <h1 className="cover-heading">Cover your page.</h1>
+      <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+      <p className="lead">
+        <a href="#" className="btn btn-lg btn-secondary">Learn more</a>
+      </p>
+    </main>)
 }
